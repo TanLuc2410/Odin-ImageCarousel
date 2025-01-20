8ab9b953b42a50912b18385dc3af825aeac62a95
Tan Luc <149952439+TanLuc2410@users.noreply.github.com>
2025-01-20 17:02:12 +0700

    Image Carousel with CSS only

--- src/index.js
+++ src/index.js
@@ -8,13 +8,16 @@ const imageCarousel = () => {
   const forwardBtn = document.querySelector(".forwardBtn");
   const backBtn = document.querySelector(".backBtn");
 
+  const dotContainer = document.querySelector(".dots");
+
   let currentSlide = 0;
   const LAST_IMAGE = imgArray.length - 1;
   const FIRST_IMAGE = 0;
 
   const nextSlide = () => {
     if (currentSlide < LAST_IMAGE) currentSlide += 1;
-    for (let i = 1; i < imgArray.length; i++) {
+    ////// CAN ADD AN IF STATEMENT HERE IF I WANT TO INTERVAL LOOP AGAIN AFTER REACHING THE LAST IMAGE
+    for (let i = 0; i < imgArray.length; i++) {
       if (i === currentSlide) {
         imgContainer.style.transform = `translateX(-${i * 86}%)`;
       }
@@ -32,6 +35,14 @@ const imageCarousel = () => {
 
   forwardBtn.addEventListener("click", nextSlide);
   backBtn.addEventListener("click", previousSlide);
+  dotContainer.addEventListener("click", (e) => {
+    const previousActiveDot = document.querySelector(".active");
+    if (previousActiveDot) previousActiveDot.classList.remove("active");
+    e.target.classList.toggle("active");
+    imgContainer.style.transform = `translateX(-${Number(e.target.dataset.dot) * 86}%)`;
+    currentSlide = Number(e.target.dataset.dot);
+  });
+  setInterval(nextSlide, 5000);
 };
 
 imageCarousel();
